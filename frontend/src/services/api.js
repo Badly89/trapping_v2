31b6fa58d61a3b1b6089d116bd7a22ee02802671
Aleks <malykhinal@mail.ru>
2026-04-10 16:09:59 +0500

    xxxx

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API_BASE_URL = 'http://10.87.0.59:6005/api';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:6005/api';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
